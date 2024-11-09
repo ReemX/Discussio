8fb3d78c6112ad1c055464091b309369eb6d5a63
Reem Assaf <35267832+ReemX@users.noreply.github.com>
2024-11-09 09:19:03 +0200

    Changed Name.

--- src/addon.ts
+++ src/addon.ts
@@ -85,7 +85,7 @@ const showNameCache = new Map<string, string>();
 const builder = new addonBuilder({
   id: "com.discussio",
   version: "1.0.1",
-  name: "Discussio",
+  name: "Discussio | ElfHosted",
   description: `Opens Google search for TV show episode discussions with one click. Simply select an episode to search for its discussions online. \n\nHosted by ElfHosted!`,
   resources: ["stream"],
   types: ["series"],
